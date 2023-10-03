606a4f6e353ab0e01911a8131d1b736640d66b42
HTTP404 <46433848+HTTP404Not-Found@users.noreply.github.com>
2023-10-03 09:51:49 +0800

    .

---
+++ M1Q19.py
@@ -0,0 +1,7 @@
+un1 = int(input())
+ch = int(input())
+
+if ch == 1 :
+    print(round((un1 -80) * 0.7,1))
+if ch == 2:
+    print(round((un1 -70) * 0.6,1))
--- M1Q2.py
+++ M1Q2.py
@@ -1,2 +1,2 @@
 un1 = input()
-print( round(float(un1) * 9/5 +32 , 2) ,'\n' )
+print(round(float(un1) * 9/5 +32 , 2) ,'\n' )
